1dbd70fdea7a471fd3bd7fec88d21f4d75710b23
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2021-12-14 13:20:36 +0000

    cursor behaviour in file update

--- kilo.c
+++ kilo.c
@@ -221,13 +221,25 @@ void abFree(struct abuf *ab) {
 }
 
 void editorMoveCursor(int key) {
+
+	erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
 	switch(key) {
 		case ARROW_LEFT:
-			if (E.cx != 0)
+			if (E.cx != 0) {
 				E.cx--;
+			} else if (E.cy > 0) {
+				E.cy--;
+				E.cx = E.row[E.cy].size;
+			}
 			break;
 		case ARROW_RIGHT:
-			E.cx++;
+			if (row && E.cx < row->size) {
+				E.cx++;
+			} else if (row && E.cx == row->size) {
+				E.cy++;
+				E.cx = 0;
+			}
 			break;
 		case ARROW_UP:
 			if (E.cy != 0)
@@ -238,6 +250,13 @@ void editorMoveCursor(int key) {
 				E.cy++;
 			break;
 	}
+
+	row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+	int rowlen = row ? row->size : 0;
+	if (E.cx > rowlen) {
+		E.cx = rowlen;
+	}
+
 }
 
 void editorProcessKeypress() {
